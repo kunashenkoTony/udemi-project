648d82f62d6870f9b793d47ad5c515fdf5f78f37
Anton <kunashenko.ant@gmail.com>
2019-07-09 15:22:10 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+
+
+
+
+'use strict'; // Обозначет что мы пишем на стандарте ES6
+
+var namber = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+let something
+console.log(something);
+
+let incr = 10,
+    decr = 10;
+
+    console.log(incr++);
+    console.log(decr--);
+    console.log('2'=== 2);
+
+    let isChecked = true,
+        isClouse = true;
+
+        console.log(isChecked && isClouse ); // Амперсант - логический оператор 'и'
+        console.log(isChecked || isClouse ); // Оератор или 
+        console.log(isChecked || !isClouse ); // || !isClouse Знак отрицания
+
+
+
+
